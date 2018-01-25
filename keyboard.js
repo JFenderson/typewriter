227f31fdefc6e6de3ab3c3dc0158d617dd3e8f2e
JFenderson <fenderson.joseph@gmail.com>
2018-01-25 16:59:21 -0600

    get highlighted keys

--- keyboard.js
+++ keyboard.js
@@ -1,101 +1,80 @@
-$(document).ready(function(){
-    //console.log("ready!");
-var keys = []
-var $hlight = $('.key'), $span = $('span'),$space = $('#32');
 
-var ids = $('.key').map(function(){
-    return $(this).attr('id');
+let sentences = ['ten ate neite ate nee enet ite ate inet ent eate',' Too ato too nOt enot one totA not anot tOO aNot',' oat itain oat tainnate eate tea anne inant nean',' itant eate anot eat nato inate eat anot tain eat', ' nee ene ate ite tent tiet ent ine ene ete ene ate'];
+let sentencesIndex = sentences.length;
+let sentenceContainer = $('#sentence-container');
+console.log(sentencesIndex);
+let sentenceCounter = 0;
+
+$("#keyboard-upper-container").hide();
+
+$(document).keydown(function(e) {
+    if(e.which === 16){
+        $("#keyboard-upper-container").show();
+        $("#keyboard-lower-container").hide();
+    }
 });
-console.log(ids);
 
-    // var i = 0;
-    // var ids = [];
-    // $(".class").each(function () {
-    //     ids[i++] = $(this).attr("id"); //this.id
-    // });
+$(document).keyup(function(e) {
+    if (e.which === 16){
+        $("#keyboard-upper-container").hide();
+        $("#keyboard-lower-container").show();
+    }
+    
+});
 
+$(document).bind("keydown", function(e){
+    $("#" + (event.which)).css("background-color", "green");
+});
+$(document).bind("keyup", function (e) {
+    $("#" + (event.which)).css("background-color", "");
+});
+$(document).bind("keydown", function (e) {
+    $("#" + (event.which + 32)).css("background-color", "green");
+});
+$(document).bind("keyup", function (e) {
+    $("#" + (event.which + 32)).css("background-color", "");
+});
+$(document).bind("keydown", function (e) {
+    $("#32" + (event.which)).css("background-color", "green");
+});
+$(document).bind("keyup", function (e) {
+    $("#32" + (event.which)).css("background-color", "");
+});
 
-    $(document).keydown(function(e) {
-        if(e.keyCode === 16){
-            $("#keyboard-upper-container").toggle();
-            $("#keyboard-lower-container").toggle();
-            
-        }
-        keys[e.keyCode] = e.type == 'keydown';
+const sentence = $(`<span>${sentences[0]}</span>`);
+sentenceContainer.append($())
 
-        //var l = keys.length;
-            // for (i = 0; i < ids.length; i++) {
-            //     if (ids[i]) {
-            //         $('ids').toggleClass('highlight');
-            //         console.log(i + "pressed");
-            //     }
-            // } 
-        $hlight.toggleClass('highlight','#32');
-
-          
-            //console.log("b has been pressed");
-        
+const configureSentence = (sentence) => {
+    const splitSentence = sentence.split('')
+    .join
+    return splitSentence.map((character) => {
+        return `<span id="target->${index}">${character}</span>`;
     });
-
-    $(document).keyup(function(e) {
-        // var $hlight = $('span.key'), $span = $('span'), $space = $('#32');
-        if (e.keyCode === 16){
-            $("#keyboard-upper-container").toggle();
-            $("#keyboard-lower-container").toggle();
-        }
-        keys[e.keyCode] = e.type == 'keyup';
-        // var l = keys.length;
-        //     for(i = 0;i < l; i++){
-        //         if(keys[i]){
-        //             $('keys[i]').toggleClass('highlight')
-        //             console.log(i + "no pressed");
-        //         }
-        //     } 
-        $hlight.toggleClass('highlight','#32');
-            //console.log("class added");
-
-       
-    });
+    // let l = 'text';
+    // l.split('');
+});
 
 
-    // var map = {}; // You could also use an array
-    // onkeydown = onkeyup = function (e) {
-    //     e = e || event; // to deal with IE
-    //     map[e.keyCode] = e.type == 'keydown';
-    //     /* insert conditional here */
-    //}
+let queue = sentences;
+$(document).keydown(function(e){
+    if(event.which !== 0) {
+        let val = queue.shift();
+        $("#sentence").append(val);
+    }
+});
+
+const highlightTargetLetter = () => {
+
+}
 
 
 
+// $(document).keypress(function(e){
+//     for (i = 0; i < ids.length; i++)
+//     if(typeof ids == "number" && isFinite(id)){
+//         console.log("true")
+
+//     }
+// })
 
 
-
-        // var l = key.length;
-        //         for(i = 0;i < l; i++){
-        //             if(keys[i]){
-        //                 console.log(i + "pressed");
-        //             }
-        //         } 
-
-
-    // function changeKeys() {
-    //     var html = '';
-    //     for (var i in keys) {
-    //         if(!keys.hasOwnProperty(i)) continue;
-            
-    //     }
-    // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
