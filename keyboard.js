50124b49c02b098d5da041bd710fd8693ae4f454
JFenderson <fenderson.joseph@gmail.com>
2018-01-26 16:58:19 -0600

    homework

--- keyboard.js
+++ keyboard.js
@@ -1,9 +1,9 @@
 
 let sentences = ['ten ate neite ate nee enet ite ate inet ent eate',' Too ato too nOt enot one totA not anot tOO aNot',' oat itain oat tainnate eate tea anne inant nean',' itant eate anot eat nato inate eat anot tain eat', ' nee ene ate ite tent tiet ent ine ene ete ene ate'];
-let sentencesIndex = sentences.length;
-let sentenceContainer = $('#sentence-container');
-console.log(sentencesIndex);
+let sentenceContainer = $('#sentence');
 let sentenceCounter = 0;
+let queue = sentences;
+
 
 $("#keyboard-upper-container").hide();
 
@@ -11,70 +11,64 @@ $(document).keydown(function(e) {
     if(e.which === 16){
         $("#keyboard-upper-container").show();
         $("#keyboard-lower-container").hide();
+    }if (e.keyCode + 32) {
+        $("#" + (event.which + 32)).css("background-color", "green"); 
     }
+    $("#" + (event.which)).css("background-color", "green");
+    $("#32" + (event.which)).css("background-color", "green");
+    for(var i in splitSentence);
+    
+        sentenceCounter++;
+        //console.log(sentenceCounter)
 });
 
 $(document).keyup(function(e) {
     if (e.which === 16){
         $("#keyboard-upper-container").hide();
         $("#keyboard-lower-container").show();
+    } if (e.keyCode + 32) {
+        $("#" + (event.which + 32)).css("background-color", "");
     }
+    $("#32" + (event.which)).css("background-color", "");
+    $("#" + (event.which)).css("background-color", "");
+
+
+});
+let configureSentence = sentences.map((sentence)=>{
+    return sentence.split(' ');
     
 });
 
-$(document).bind("keydown", function(e){
-    $("#" + (event.which)).css("background-color", "green");
-});
-$(document).bind("keyup", function (e) {
-    $("#" + (event.which)).css("background-color", "");
-});
-$(document).bind("keydown", function (e) {
-    $("#" + (event.which + 32)).css("background-color", "green");
-});
-$(document).bind("keyup", function (e) {
-    $("#" + (event.which + 32)).css("background-color", "");
-});
-$(document).bind("keydown", function (e) {
-    $("#32" + (event.which)).css("background-color", "green");
-});
-$(document).bind("keyup", function (e) {
-    $("#32" + (event.which)).css("background-color", "");
-});
-
-const sentence = $(`<span>${sentences[0]}</span>`);
-sentenceContainer.append($())
-
-const configureSentence = (sentence) => {
-    const splitSentence = sentence.split('')
-    .join
-    return splitSentence.map((character) => {
-        return `<span id="target->${index}">${character}</span>`;
-    });
-    // let l = 'text';
-    // l.split('');
-});
 
 
-let queue = sentences;
-$(document).keydown(function(e){
-    if(event.which !== 0) {
-        let val = queue.shift();
-        $("#sentence").append(val);
-    }
-});
+let splitSentence = configureSentence.map((character,index) => {
+            return `<span id="target-${index}">${character}</span>`
+        }).join('');
 
-const highlightTargetLetter = () => {
+//console.log(splitSentence);
 
-}
+sentenceContainer.append(splitSentence);
+console.log(splitSentence);
+let sentencesIndex = splitSentence.length;
 
-
-
-// $(document).keypress(function(e){
-//     for (i = 0; i < ids.length; i++)
-//     if(typeof ids == "number" && isFinite(id)){
-//         console.log("true")
-
+// const highlightTargetLetter = () => {
+//     if(e.which >= 97){
+//         console.log("highlight");
 //     }
-// })
+// }
+
+
+// console.log(sentenceCounter);
+// console.log(configureSentence);
+// console.log();
+
+
+
+// $(document).keydown(function(e){
+//     if(event.which !== 0) {
+//         let val = queue.shift();
+//         $("#sentence").append(val);
+//     }
+// });
 
 
